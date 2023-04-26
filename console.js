1a110f4b62a995794397a1dedfa2180c5d76ab20
PRADIP BHATT <bhattsammar04@gmail.com>
2023-04-26 16:14:40 +0545

    last commit

---
+++ console.js
@@ -0,0 +1,15 @@
+// equal operator
+console.log( 5 == 5 );
+console.log( 10 == 10 );
+
+// not equal operator
+console.log( 10 != 5 );
+console.log( 4!= 4);
+
+// strict equal operator
+console.log( 5 === '5' );
+console.log( 5=== 5 );
+
+// strict not equal operator
+console.log( 5 !== '6' );
+console.log( 5 !== 5 );
