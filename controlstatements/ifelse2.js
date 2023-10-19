df4be13b7840ec70108795ad44e629f821f0d881
Sapana Pandey <sapanapandey447@gmail.com>
2023-10-19 20:46:10 +0545

    if else code

---
+++ controlstatements/ifelse2.js
@@ -0,0 +1,26 @@
+//basic code for the implementation of if else logic
+let a=55;
+if(a>=90 && a<=100)
+{
+    console.log("A+ yayyy");
+}
+else if(a>=80 && a<=90)
+{
+    console.log("A good job");
+}
+else if(a>=70 && a<=80)
+{
+    console.log("B+ good");
+}
+else if(a>=60 && a<=70)
+{
+    console.log("B okay");
+}
+else if(a>=50 && a<=60)
+{
+    console.log("C mid");
+}
+else
+{
+    console.log("you failed haha loser");
+}
