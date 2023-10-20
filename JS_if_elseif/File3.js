2ca35593ec0f56f9a3b744a096c706506a6884fd
Babita Bhatt <babitabhatta37@gmail.com>
2023-10-19 21:54:18 -0500

    i added voting system

---
+++ JS_if_elseif/File3.js
@@ -0,0 +1,9 @@
+let eligibleage = prompt("enter ur age=");
+if(eligibleage>=18)
+{
+    document.write(" you can vote in election");
+
+}
+else{
+    document.write("you are not eligible to vote");
+}
