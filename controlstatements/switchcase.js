907f71b1807a83f3d2cde05c2961e59a8ad36798
Sapana Pandey <sapanapandey447@gmail.com>
2023-10-29 21:50:12 +0545

    use of switch statement in js

---
+++ controlstatements/switchcase.js
@@ -0,0 +1,17 @@
+//switch statement in js
+let a = 3
+switch (a) {
+    case 1:
+        a = "one";
+        break;
+    case 2:
+        a = "two";
+        break;
+    case 3:
+        a = "three";
+        break;
+    default:
+        a = "not found";
+        break
+}
+console.log("the value of a is", a);
