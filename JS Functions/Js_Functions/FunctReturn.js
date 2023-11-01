21f0d342f4d622d009bf2a3777f0c9ac2090b6b6
Sapana Pandey <sapanapandey447@gmail.com>
2023-11-01 08:37:06 +0545

    functions in js

---
+++ JS Functions/Js_Functions/FunctReturn.js
@@ -0,0 +1,15 @@
+//returning value from a function
+function sum(a,b) //declaration
+{
+    return sum=a+b; 
+}
+let s= sum(3,4); //function call
+console.log("the sum is:", s);
+
+//to find square of a number
+
+let a= function(num)
+{
+    return square= num*num;
+}
+console.log("the square of number is",a(5));
---
+++ JS Functions/Js_Functions/JSfunct.js
@@ -0,0 +1,22 @@
+// function greet() //function declaration
+// {   //body of function
+//     console.log("heyy there"); 
+// }
+// greet(); //function call, unless called flow won't go inside function body
+
+
+// function greet(name){
+//     console.log("heyy"+ name +":)");
+// }
+// let name=prompt("enter a name:");
+// greet(name);
+
+
+// adding two numbers using function call
+
+function sum(a,b)
+{
+    console.log("the sum is:", a+b);
+
+}
+sum(2,4);
