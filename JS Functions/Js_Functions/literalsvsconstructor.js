fbbf291a6d6ba4974688f7c16336483f3ab1ffc5
Sapana Pandey <sapanapandey447@gmail.com>
2023-11-06 21:27:45 +0545

    constructors vs object literals

---
+++ JS Functions/Js_Functions/literalsvsconstructor.js
@@ -0,0 +1,15 @@
+//using object literals
+// let person={
+//     name:"sapana"
+// }
+//using constructor
+
+function Person(){  
+    this.name="sapana"
+}
+let person1=new Person();
+console.log(person1.name);
+
+//difference is that object literal is used to create 
+//single object but with constructor we can create 
+//multiple objects
