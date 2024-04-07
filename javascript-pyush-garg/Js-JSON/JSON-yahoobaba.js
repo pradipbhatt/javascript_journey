5bbdab60f971fd05889a2df63e5c69c1b8abfcb3
Pradip Bhatt <bhattsammar04@gmail.com>
2024-04-07 09:49:45 -0700

    fix(JSON):JSON practises

---
+++ javascript-pyush-garg/Js-JSON/JSON-yahoobaba.js
@@ -0,0 +1,26 @@
+// Used to store abd exchange the data from server to client .
+// Yesko Javascript sanga kehi relation hunna
+// Yesle server rw client ma fast data transfer garne kaam garcha text format ma yesle video audio support gardaina send garna.
+// Format
+let pradip = {
+    "name":"PradipBhatt",
+    "email":"pradip10bhatt@gmail.com",
+    "age":23,
+    "gender":"Male"
+};
+
+console.log(pradip);   //Returns a JSON representation of the list.
+
+// Javascript Objects example
+const Students = {
+    name :"pradip",
+    age:23,
+    gender:"Male"
+};
+
+console.log(Students);  //return Object
+
+// Rule --- yesma always double quotes use garnu parcha
+
+
+// API --Application Program Interface,suppose we've to perform CRUD on server and client side first client side sends request to server and server sends response to client side.
