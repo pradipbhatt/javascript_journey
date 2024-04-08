e4893bb64e11ea542796af412b503a1a02fab14c
Pradip Bhatt <bhattsammar04@gmail.com>
2024-04-07 19:12:58 -0700

    Reached in practise set 4 chapter 3 of loops and functons

---
+++ javascript-pyush-garg/Practise-Set/practise-set-4.js
@@ -0,0 +1,9 @@
+// WAP to print the marks of the student in an object using the for loop
+const pradip ={
+    name:"Pradip",
+    marks:[20,30,40,50,60,70,80,90,100],
+}
+
+for(let i=0; i<pradip.marks.length; i++){
+    console.log(pradip.marks[i]);
+}
